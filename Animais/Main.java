96b44096de9b8723cf09dfdbd2eaf70c87f64c10
isaDelboni <162186706+isaDelboni@users.noreply.github.com>
2024-04-11 21:36:50 -0300

    Update Main.java

--- Animais/Main.java
+++ Animais/Main.java
@@ -3,12 +3,31 @@ public class Main {
         
         Animal animal =new Animal();
         Cachorro cachorro = new Cachorro();
+        Gato gato = new Gato();
+        Vaca vaca = new Vaca();
 
-        animal.setNome("Chico");
-
+        System.out.println("--Cachorro: ");
+        cachorro.setNome("Chico");
         cachorro.setRaca("Boxer");
         cachorro.setNome("Rex");
+        cachorro.setPorte("Grande");
+        cachorro.setIdade(11);
+        cachorro.setSexo("Masculino");
 
+        System.out.println("--Gato:");
+        gato.setNome("Luna");
+        gato.setIdade(4);
+        gato.setSexo("Femêa");
+        gato.setPelagem("Laranja");
+        gato.setCacando(false);
+        gato.mudarCacando(false);
+
+        System.out.println("--Vaca: ");
+        vaca.setNome("Mimosa");
+        vaca.setIdade(13);
+        vaca.setSexo("Femêa");
+        vaca.setDieta("Pasto");
+        vaca.setManchas("Marrons");
 
         System.out.println(animal);
         System.out.println(cachorro);
