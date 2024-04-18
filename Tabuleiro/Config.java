51f63a6b1cd4a29446fe311e5c4712d39806cdd6
isaDelboni <162186706+isaDelboni@users.noreply.github.com>
2024-04-18 16:05:49 -0300

    Update Config.java

--- Tabuleiro/Config.java
+++ Tabuleiro/Config.java
@@ -1,56 +1,30 @@
+import java.util.List;
+
 public class Config {
 
-    public static void imprimirTabuleiro(String tabuleiro[][]){
-        for(int i=0; i<tabuleiro.length; i++){
-            for(int j=0; j<tabuleiro[i].length; j++){
-                System.out.print("["+ tabuleiro[i][j]+"]");
+    public static void imprimirTabuleiro(String tabuleiro[][]) {
+
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print("[" + tabuleiro[i][j] + "]");
             }
             System.out.println();
         }
     }
 
-    public static void moverDireita(String tabuleiro[][]){
-        for(int i=0; i<tabuleiro.length; i++){
-            for(int j=0; j<tabuleiro[i].length; j++){
-                if(tabuleiro[i][j].equals("O")){
-                    tabuleiro[i][j] = "X";
-                    tabuleiro[i+1][j] = "O";
-                }
+    public static String[][] atualizarTabuleiro(List<Jogador> jogadores) {
+
+        String[][] tabuleiro = new String[10][10];
+
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                tabuleiro[i][j] = ".";
             }
         }
-    }
 
-    public static void moverEsquerda(String tabuleiro[][]){
-        for(int i=0; i<tabuleiro.length; i++){
-            for(int j=0; j<tabuleiro[i].length; j++){
-                if(tabuleiro[i][j].equals("O")){
-                    tabuleiro[i][j] = "X";
-                    tabuleiro[i-1][j] = "O";
-                }
-            }
+        for (Jogador jogador : jogadores) {
+            tabuleiro[jogador.getX()][jogador.getY()] = jogador.getRepresentacao();
         }
+        return tabuleiro;
     }
-
-    public static void moverCima(String tabuleiro[][]){
-        for(int i=0; i<tabuleiro.length; i++){
-            for(int j=0; j<tabuleiro[i].length; j++){
-                if(tabuleiro[i][j].equals("O")){
-                    tabuleiro[i][j] = "X";
-                    tabuleiro[i][j-1] = "O";
-                }
-            }
-        }
-    }
-
-    public static void moverBaixo(String tabuleiro[][]){
-        for(int i=0; i<tabuleiro.length; i++){
-            for(int j=0; j<tabuleiro[i].length; j++){
-                if(tabuleiro[i][j].equals("O")){
-                    tabuleiro[i][j] = tabuleiro[i][j+1];
-                }
-            }
-        }
-    }
-
 }
-
