8a49e278b5f85ef6f317e398a358c3a2bf8d51cd
isaDelboni <162186706+isaDelboni@users.noreply.github.com>
2024-04-18 21:34:11 -0300

    Update Personagem.java

--- Xadrez/Personagem.java
+++ Xadrez/Personagem.java
@@ -1,1 +1,28 @@
+public class Personagem {
+    
+    private int posX; 
+    private int posY;
+    private String desenho;
+
+    Personagem (int posX, int posY, String desenho){
+        this.posX=posX;
+        this.posY=posY;
+        this.desenho= desenho;
+    }
+
+    public String getDesenho() {
+        return desenho;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+       
+}
+
 
