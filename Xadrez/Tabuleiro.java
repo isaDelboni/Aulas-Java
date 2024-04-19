31e5182730571152679005cfd77bd408632da0a5
isaDelboni <162186706+isaDelboni@users.noreply.github.com>
2024-04-18 21:33:51 -0300

    Update Tabuleiro.java

--- Xadrez/Tabuleiro.java
+++ Xadrez/Tabuleiro.java
@@ -1,1 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tabuleiro {
+
+    public static void tabuleiro(int largura, int altura, List<Personagem> perso) {
+        for (int i=0; i<altura; i++){
+            for (int j=0; j<largura; j++){
+                boolean Tf = false;
+
+                for(Personagem p: perso){
+                    if (j == p.getPosX() && i == p.getPosY()) {
+                        System.out.print("[" + p.getDesenho() + "]");
+                        Tf= true;
+                    }
+                }
+                if(!Tf){
+                    System.out.print("[.]");
+                }
+            }
+            
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        List<Personagem> perso = new ArrayList<>();
+
+        perso.add(new Personagem(1, 2, "x"));
+        perso.add(new Personagem(2, 1, "w"));
+        perso.add(new Personagem(3, 6, "z"));
+    
+        tabuleiro(10, 10, perso);
+
+        
+    }
+}
 
